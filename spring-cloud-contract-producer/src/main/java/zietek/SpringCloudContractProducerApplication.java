51b5b6317d43db177ac2454fca047f7927d27635
zietek <piotr.zietek@gmail.com>
2021-11-25 22:48:01 +0100

    pom

---
+++ spring-cloud-contract-producer/src/main/java/zietek/SpringCloudContractProducerApplication.java
@@ -0,0 +1,13 @@
+package zietek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudContractProducerApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-contract-producer/src/main/java/zietek/contract/EvenOddController.java
@@ -0,0 +1,14 @@
+package zietek.contract;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EvenOddController {
+
+    @GetMapping("/api/validate/number")
+    public String isNumberPrime(@RequestParam("number") String number) {
+        return Integer.parseInt(number) % 2 == 0 ? "Even" : "Odd";
+    }
+}
